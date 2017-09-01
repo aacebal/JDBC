870a35d331ba7bb3ec3be8810e3df508fe6eb08e
Adel Acebal <adelacebal@yahoo.com>
2017-09-01 18:49:57 -0400

    information added

--- TestDB/src/com/adelacebal/Main.java
+++ TestDB/src/com/adelacebal/Main.java
@@ -14,9 +14,16 @@ public class Main {
 //            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
         try {
             Connection conn = DriverManager.getConnection("jdbc:sqlite:../TestDB/testjava.db");
+//            conn.setAutoCommit(false);
             Statement statement = conn.createStatement();
-            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
-
+            statement.execute("CREATE TABLE IF NOT EXISTS contacts " +
+                                   "(name TEXT, phone INTEGER, email TEXT)");
+            statement.execute("INSERT INTO contacts (name, phone, email) " +
+                                   "VALUES('Joe', 3056689112, 'joe@email.com')");
+            statement.execute("INSERT INTO contacts (name, phone, email) " +
+                                   "VALUES('James', 305315552, 'james@email.com')");
+            statement.execute("INSERT INTO contacts (name, phone, email) " +
+                                   "VALUES('Valeria', 3058622267, 'valeria@email.com')");
             statement.close();
             conn.close();
 

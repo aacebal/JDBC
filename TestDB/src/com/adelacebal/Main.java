3d317a76e1b6b75d82a3d97589a551bef09c4a1f
Adel Acebal <adelacebal@yahoo.com>
2017-09-01 18:34:36 -0400

    table added

--- TestDB/src/com/adelacebal/Main.java
+++ TestDB/src/com/adelacebal/Main.java
@@ -3,13 +3,16 @@ package com.adelacebal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
     public static void main(String[] args) {
 
         try {
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:~/Desktop/lessons/JDBC/TestDB/testjava.db");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:../TestDB/testjava.db");
+            Statement statement = conn.createStatement();
+            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
         } catch(SQLException e) {
             System.out.println("Something went wrong " + e.getMessage());
         }

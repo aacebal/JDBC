9765efd65325095003abafab7d3a8444aef076d9
Adel Acebal <adelacebal@yahoo.com>
2017-09-01 18:19:37 -0400

    connected to database

--- TestDB/src/com/adelacebal/Main.java
+++ TestDB/src/com/adelacebal/Main.java
@@ -1,8 +1,17 @@
 package com.adelacebal;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:~/Desktop/lessons/JDBC/TestDB/testjava.db");
+        } catch(SQLException e) {
+            System.out.println("Something went wrong " + e.getMessage());
+        }
     }
 }

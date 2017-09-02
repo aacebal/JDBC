2b29f5599d3424675f7c0af7e25a23f21eb45258
Adel Acebal <adelacebal@yahoo.com>
2017-09-02 12:58:11 -0400

    starting to work with music db

--- Music/src/com/adelacebal/Main.java
+++ Music/src/com/adelacebal/Main.java
@@ -1,8 +1,15 @@
 package com.adelacebal;
 
+import com.adelacebal.model.DataSource;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        DataSource dataSource = new DataSource();
+        if (!dataSource.open()) {
+            System.out.println("Can't open data source");
+            return;
+        }
+        dataSource.close();
     }
 }
